d8258013502af670a208079f1778472df4dcb97b
Gabriel Soares <gabrielalcantarars@gmail.com>
2025-08-27 01:33:12 -0300

    fix: Date time validation when updating appointment tests

--- src/test/java/com/hospital_app/appointment_service/application/service/appointment/UpdateAppointmentUseCaseImplTest.java
+++ src/test/java/com/hospital_app/appointment_service/application/service/appointment/UpdateAppointmentUseCaseImplTest.java
@@ -2,6 +2,7 @@ package com.hospital_app.appointment_service.application.service.appointment;
 
 import com.hospital_app.appointment_service.application.port.out.db.CustomAppointmentRepository;
 import com.hospital_app.appointment_service.domain.exception.AppointmentNotFoundException;
+import com.hospital_app.appointment_service.domain.exception.InvalidAppointmentDateTimeException;
 import com.hospital_app.appointment_service.domain.model.Appointment;
 import com.hospital_app.appointment_service.domain.model.AppointmentStatus;
 import org.junit.jupiter.api.Test;
@@ -14,7 +15,8 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -36,18 +38,43 @@ class UpdateAppointmentUseCaseImplTest {
     }
 
     @Test
+    void shouldNotUpdateAppointmentWhenDateTimeIsBeforeExistingDateTime() {
+        UUID id = UUID.randomUUID();
+        Appointment inputAppointment = getAppointment();
+        Appointment existingAppointment = getAppointment();
+        existingAppointment.setDateTime(LocalDateTime.now().plusDays(1));
+        inputAppointment.setDateTime(LocalDateTime.now().minusDays(1));
+        when(customAppointmentRepository.findById(any(UUID.class))).thenReturn(Optional.of(inputAppointment));
+        assertThrows(InvalidAppointmentDateTimeException.class, () -> updateAppointmentUseCase.execute(inputAppointment, id));
+    }
+
+    @Test
+    void shouldNotUpdateAppointmentWhenDateTimeIsEqualsToExistingDateTime() {
+        UUID id = UUID.randomUUID();
+        Appointment inputAppointment = getAppointment();
+        Appointment existingAppointment = getAppointment();
+        LocalDateTime now = LocalDateTime.now();
+        existingAppointment.setDateTime(now.plusDays(1));
+        inputAppointment.setDateTime(now.plusDays(1));
+        when(customAppointmentRepository.findById(any(UUID.class))).thenReturn(Optional.of(inputAppointment));
+        assertThrows(InvalidAppointmentDateTimeException.class, () -> updateAppointmentUseCase.execute(inputAppointment, id));
+    }
+
+    @Test
     void shouldUpdateAppointment() {
         UUID id = UUID.randomUUID();
-        Appointment appointment = getAppointment();
-        when(customAppointmentRepository.findById(any(UUID.class))).thenReturn(Optional.of(appointment));
-        when(customAppointmentRepository.update(any(Appointment.class))).thenReturn(appointment);
-        Appointment updatedUser = updateAppointmentUseCase.execute(appointment, id);
+        Appointment inputAppointment = getAppointment();
+        Appointment existingAppointment = getAppointment();
+        inputAppointment.setDateTime(LocalDateTime.now().plusDays(1));
+        existingAppointment.setDateTime(LocalDateTime.now().minusDays(1));
+        when(customAppointmentRepository.findById(any(UUID.class))).thenReturn(Optional.of(existingAppointment));
+        when(customAppointmentRepository.update(any(Appointment.class))).thenReturn(existingAppointment);
+        Appointment updatedUser = updateAppointmentUseCase.execute(inputAppointment, id);
         assertNotNull(updatedUser);
     }
 
     private static Appointment getAppointment() {
         Appointment appointment = new Appointment();
-        appointment.setDateTime(LocalDateTime.now());
         appointment.setNotes("Updating notes...");
         appointment.setStatus(AppointmentStatus.COMPLETED);
         return appointment;
